95cc8e84553d80c065994aabeec8f734a41f142c
Joe Asam <joeasam@gmail.com>
2025-08-03 21:34:16 +0100

    Updated a few things

--- main.js
+++ main.js
@@ -4,3 +4,13 @@
   document.addEventListener('mousemove', (e) => {
     mouseBg.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
   });
+
+
+  const navItems = document.getElementById("nav-items");
+  const sideNav = document.getElementById("side-nav");
+  const hamburger = document.getElementById("hamburger");
+
+  hamburger.addEventListener("click", () => {
+    navItems.classList.toggle("active");
+    sideNav.classList.toggle("active");
+  })
